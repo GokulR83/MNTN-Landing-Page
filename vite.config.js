1b8ac40b7adbf3aa5f2ecc96653e170853c48e4e
GOKUL R <rajugokul003@gmail.com>
2024-02-14 05:37:18 +0530

    third commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/mntn-landing-page/",
+  base:"/MNTN-Landing-Page/",
   plugins: [react()],
 })
